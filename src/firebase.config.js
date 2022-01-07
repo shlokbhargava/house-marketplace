1c3ce3319a695f6d4fd044dd0b26a22cac91e60c
shlokbhargava <shlokchamp88@gmail.com>
2022-01-07 22:25:44 +0530

    setting basic routes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: "house-marketplace-9adf4.appspot.com",
   messagingSenderId: "1060986485018",
   appId: "1:1060986485018:web:5141132d5b1756527d94a3"
-};
+}
 
 
 initializeApp(firebaseConfig)
